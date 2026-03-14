9d9b0778869960e69b19a717fdddddb5ec7268e8
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-03-14 17:36:22 -0400

    added more methods with abstraction

--- DiscountCalculatorV2.py
+++ DiscountCalculatorV2.py
@@ -8,6 +8,14 @@ class Product:
     
     def __str__(self) -> str:
         return f"{self.name} - ${self.price}"
-    
+
+class DiscountStrategy(ABC):
+    @abstractmethod
+    def is_applicable(self, product: Product, user_tier: str) -> bool:
+        pass
+
+    @abstractmethod
+    def apply_discount(self, product: Product) -> float:
+        pass
 product = Product('Wireless Mouse', 50.0)
 print(product)
