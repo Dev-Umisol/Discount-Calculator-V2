9132e5988465a08b9c687faded8de4b8b8d29935
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-03-15 12:24:27 -0400

    created 2 more classes to determine a percent discount amnd a fixed amount discount

--- DiscountCalculatorV2.py
+++ DiscountCalculatorV2.py
@@ -18,5 +18,34 @@ class DiscountStrategy(ABC):
     @abstractmethod
     def apply_discount(self, product: Product) -> float:
         pass
+    
+class PercentageDiscount(DiscountStrategy):
+    def __init__(self, percent: int) -> None:
+        self.percent = percent
+    
+    def is_applicable(self, product: Product, user_tier: str) -> bool:
+        if self.percent <= 70:
+            return True
+        else:
+            return False
+    
+    def apply_discount(self, product: Product) -> float:
+        return product.price * (1 - self.percent / 100)
+
+class FixedAmountDiscount(DiscountStrategy):
+    def __init__(self, amount: int) -> None:
+        self.amount = amount
+    
+    def is_applicable(self, product: Product, user_tier: str) -> bool:
+        if product.price * 0.9 > self.amount:
+            return True
+        else:
+            return False
+    
+    def apply_discount(self, product: Product) -> float:
+        return product.price - self.amount
+
 product = Product('Wireless Mouse', 50.0)
 print(product)
+discount = PercentageDiscount(10)
+print(discount.apply_discount(product))
