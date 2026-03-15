5e77d6b1ee3ed4c0ef759e11cbb875cda1712b33
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-03-15 13:26:31 -0400

    Finished Discount Calculator v2

--- DiscountCalculatorV2.py
+++ DiscountCalculatorV2.py
@@ -68,19 +68,36 @@ class PremiumUserDiscount(DiscountStrategy):
         return product.price * 0.8
     
 class DiscountEngine:
+    """ Returns the minimum price for user_tiers """
+    
     def __init__(self, strategies: list[DiscountStrategy]) -> None:
         self.strategies = strategies
         
     def calculate_best_price(self, product: Product, user_tier: str) -> float:
-        pass
+        prices = [product.price]
+        
+        for strategy in self.strategies:
+            if strategy.is_applicable(product, user_tier):
+                discounted = strategy.apply_discount(product)
+                prices.append(discounted)
+        
+        return min(prices)
 
 # --- Usage Examples ---
-product = Product('Wireless Mouse', 50.0)
-print(product)
+if __name__ == '__main__':
+    product = Product('Wireless Mouse', 50.0)
+    user_tier = 'Premium'
+    strategies = [PercentageDiscount(10), FixedAmountDiscount(5), PremiumUserDiscount()]
+    engine = DiscountEngine(strategies)
+    best_price = engine.calculate_best_price(product, user_tier)
+    
+    print(f"Best price for {product.name} for {user_tier} user: ${best_price:.2f}")
+    
+# print(product)
 
-discount = PercentageDiscount(10)
-print(discount.apply_discount(product))
+# discount = PercentageDiscount(10)
+# print(discount.apply_discount(product))
 
-fixed_discount = FixedAmountDiscount(5)
-print(fixed_discount.apply_discount(product))
+# fixed_discount = FixedAmountDiscount(5)
+# print(fixed_discount.apply_discount(product))
 
